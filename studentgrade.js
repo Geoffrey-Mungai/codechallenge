fadf78e87c06fdb88db219715381514c4728eff3
Geoffrey-Mungai <geoffrey.mungai1@student.moringaschool.com>
2024-12-18 09:00:55 +0300

    studentgrade.js

--- studentgrade.js
+++ studentgrade.js
@@ -1,18 +1,20 @@
-let mark = prompt('Enter a grade');
 
-function studentGrade(mark) {
-    if(mark >= 80 && mark <= 100){
-        return 'A';
+function gradeScore(scores){
+    
+    if (scores >= 70 && scores <=100){
+        return "Your grade is A";
+    }else if (scores >= 60 && scores <79){
+        return "Your grade is B";
+    }else if (scores >= 49 && scores <= 59){
+        return "Your grade is C";
+    }else if (scores >= 40 && scores <49){
+        return "Your grade is D";
+    }else if (scores >= 0 && scores <= 40){
+        return "Your grade is E";
+    }else {
+        return "Invalide scores,,,please try again";
     }
-    else if(mark >= 60 && mark <=80){
-        return 'B';   
-    }
-    else if(mark >=49 && mark <=60) {
-        return 'C';
-    }
-    else{
-        return 'invalid'
-     }
-    }
-    let score = prompt("Enter your scores: ");
-        alert(gradeScor(score));
+}
+//parseInt convert the input from prompt to a number because its returnned as a string
+let score = parseInt(prompt("Enter your scores: "));// It prompts the user to enter the score
+alert(gradeScore(score));
